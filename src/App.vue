795c9309fb59f1d03a5191dda9343e02ad250274
Bruno Soares <bruno.sk.8@hotmail.com>
2022-07-15 18:04:19 -0300

    First Stage

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-11/12 mx-auto bg-white">
+    <div class="rounded-t-lg">
+      <img src="./assets/images/image-product-mobile.jpg" alt="" />
+    </div>
+    <div class="w-11/12 mx-auto">
+      <p>PERFUME</p>
+      <p>Gabrielle Essence Eau De Parfum</p>
+      <p>
+        A floral, solar and voluptuous interpretation composed by Olivier Polge,
+        Perfumer-Creator for the House of CHANEL.
+      </p>
+      <div>
+        <span>$149.99</span>
+        <span>$169.99</span>
+      </div>
+      <button>Add to Cart</button>
+    </div>
+    <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+        >Frontend Mentor</a
+      >. Coded by <a href="https://github.com/felipebss1">Bruno Soares</a>.
+    </div>
+  </div>
+</template>
+<style scoped>
+.attribution {
+  font-size: 11px;
+  text-align: center;
+}
+
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+</style>
